f444ced83e9e1149a4bb71687fc9baa4cff8d76f
Steve Juarez <sjuarez3@uwo.ca>
2016-11-26 21:18:16 -0500

    made login page, using session to keep data persistence across app but I know its poo...renamed the forms so its easier to read

--- main.py
+++ main.py
@@ -1,43 +1,66 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 @app.route("/")
 def login():
-    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
-    cursor = cnx.cursor()
-    query = ("SELECT * FROM Customer")
-    cursor.execute(query)
-    customers=cursor.fetchall()
-    cnx.close()
-    return render_template('customers.html', customers=customers)
+    return render_template('login.html')
 	
-@app.route('/entercustomer')
-def addCustomer(name=None):
-    return render_template('form6.html', name=name)
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 	
-@app.route('/selectcustomer')
-def selectcustomer(name=None):
-    return render_template('form1.html', name=name)
-	
-@app.route('/ratemovie')
-def rateMovie(name=None):
-    return render_template('form4.html', name=name)
+@app.route('/viewprofile')
+def viewProfile():
+    FirstName = session.get('FirstName', None)
+    LastName = session.get('LastName', None)
+    EmailAddress = session.get('EmailAddress', None)
+    Sex = session.get('Sex', None)
+    idCustomer = session.get('idCustomer', None)
+    return render_template('customerprofile.html', FirstName=FirstName, LastName=LastName, EmailAddress=EmailAddress, Sex=Sex, idCustomer=idCustomer)
 
-@app.route('/submit', methods=["POST"])
-def submit():
+@app.route('/index', methods=["POST"])
+def index():
     cnx = mysql.connector.connect(user='root', database='MovieTheatre')
     cursor = cnx.cursor()
     insert_stmt = (
         "INSERT INTO Customer (FirstName, LastName, EmailAddress, Sex) "
-        "VALUES (%s, %s, %s, %c)"
+        "VALUES (%s, %s, %s, %s)"
     )
     data = (request.form['FirstName'], request.form['LastName'], request.form['EmailAddress'], request.form['Sex'])
     cursor.execute(insert_stmt, data)
     cnx.commit()
     cnx.close()
-    return render_template('index.html', firstname=request.form['FirstName'], lastname=request.form['LastName'], EmailAddress=request.form['EmailAddress'], Sex=request.form['Sex'])
+    return render_template('index.html', FirstName=request.form['FirstName'], LastName=request.form['LastName'], EmailAddress=request.form['EmailAddress'], Sex=request.form['Sex'])
 
+@app.route('/mainMenu', methods=["POST"])
+def mainMenu():
+    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    cursor = cnx.cursor()
+
+    FirstName = request.form['FirstName']
+    session['FirstName'] = request.form['FirstName']
+    LastName = request.form['LastName']
+    session['LastName'] = request.form['LastName']
+    EmailAddress = request.form['EmailAddress']
+    session['EmailAddress'] = request.form['EmailAddress']
+	
+    query = ("SELECT * FROM Customer WHERE FirstName = '" + FirstName + "' AND LastName = '" + LastName + "' AND EmailAddress = '" + EmailAddress + "'")
+    cursor.execute(query)
+    print("Attempting: " + query)
+    customers=cursor.fetchall()
+	
+    for row in customers:
+        idCustomer=row[0]
+        session['idCustomer']=idCustomer
+        Sex=row[4]
+        session['Sex'] = Sex
+	
+    cnx.commit()
+    cnx.close()
+    return render_template('mainmenu.html', FirstName=request.form['FirstName'], LastName=request.form['LastName'], EmailAddress=request.form['EmailAddress'], Sex=Sex)
+	
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
