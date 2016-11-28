87a47052483713f9cf2eeb5526bdc845e946771f
Steve Juarez <sjuarez3@uwo.ca>
2016-11-28 00:17:24 -0500

    added staff managment html file and list movies page

--- theatre_manager/staff.py
+++ theatre_manager/staff.py
@@ -5,3 +5,18 @@ import mysql.connector
 @app.route('/staff')
 def staff():
     return render_template('staff.html')
+	
+@app.route('/listmovies')
+def listMovies():
+    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    cursor = cnx.cursor()
+    query = (
+        "SELECT * FROM Movie ORDER BY MovieName")
+    cursor.execute(query)
+    movies=cursor.fetchall()
+    cnx.close()
+    return render_template('listmovies.html', movies=movies)
+	
+@app.route("/staffmenu", methods=["POST"])	
+def returnStaffMenu():
+    return render_template('staff.html')	
